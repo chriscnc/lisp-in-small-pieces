655484c3e3cb91cbb7918c4bafd6369a00563354
Chris Cornelison <chris.cornelison@gmail.com>
2018-01-16 21:52:27 -0600

    update

--- chap01.rkt
+++ chap01.rkt
@@ -1,5 +1,39 @@
 #lang racket
 
+(define (d.evaluate e env)
+  (if (atom? e) ; (atom? e) == (not (pair? e))
+    (cond [(symbol? e) (lookup e env)]
+          [(or (number? e) (string? e) (char? e) (boolean? e) (vector? e)) e]
+          [else (error "Cannot evaluate" e)])
+    (case (car e)
+      [(quote)  (cadr e)]
+      [(if)     (if (evaluate (cadr e) env)
+                  (evaluate (caddr e) env)
+                  (evaluate (cadddr e) env))]
+      [(begin)  (eprogn (cdr e) env)]
+      [(set!)   (update! (cadr e) env (d.evaluate (caddr e) env))]
+      [(lambda) (d.make-function (cadr e) (cddr e) env)]
+      [else     (d.invoke (d.evaluate (car e) env)
+                          (evlis (cdr e) env)
+                          env)]
+      )))
+
+(define (d.invoke fn args env) 
+  (if (procedure? fn)
+    (fn args env)
+    (error "Not a function" fn)))
+
+(define (d.make-function variables body def.env)
+  (lambda (values current.env) 
+    (eprogn body (extend current.env variables values))))
+
+
+(define env.init '())
+
+(define env.global 
+  (list (mcons '+ (lambda (values) (apply + values)))
+        (mcons 'list (lambda (values) (apply list values)))
+        ))
 
 (define (evaluate e env)
   (if (atom? e) ; (atom? e) == (not (pair? e))
@@ -14,14 +48,26 @@
       [(begin)  (eprogn (cdr e) env)]
       [(set!)   (update! (cadr e) env (evaluate (caddr e) env))]
       [(lambda) (make-function (cadr e) (cddr e) env)]
-      [else     (invoke (evaluate (car e) env)
-                        (evlis (cdr e) env))]
-      )))
+      [else     (begin
+                  (displayln "invoke...")
+                  (displayln e)
+                  (displayln "in environment...")
+                  (displayln env)
+                  (invoke (evaluate (car e) env)
+                        (evlis (cdr e) env)))])))
 
 
 (define (make-function variables body env)
   (lambda (values) 
-    (eprogn body (extend env.init variables values))))
+    (eprogn body (extend env variables values))))
+
+
+
+(define (invoke fn args) 
+  (if (procedure? fn)
+    (let [(result (fn args))]
+      result)
+    (error "Not a function" fn)))
 
 
 (define (extend env variables values)
@@ -37,11 +83,6 @@
         [(symbol? variables)
          (cons (mcons variables values) env)]))
 
-
-(define (invoke fn args) 
-  (if (procedure? fn)
-    (fn args)
-    (error "Not a function" fn)))
 
 (define (eprogn exps env)
   (if (pair? exps)
@@ -77,10 +118,6 @@
              value)
       (update! id (cdr env) value))
     (error "No such binding" id)))
-
-
-(define env.init '())
-
 
 
 
